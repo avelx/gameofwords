7d26cc94b6ec2ff1adb7e5cf4125a4f37a4659ac
avelreserch <pavel.oliynyk@gmail.com>
2020-07-06 10:47:25 +0100

    Enchance unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,22 @@
 import Test.Hspec
 import Lib 
+import Data
 
 main :: IO ()
 main = hspec $ do
- describe "How to write a test" $ do
-   it "Should be able to run tests" $ do
-     someString `shouldBe` "someString"
+ describe "formatGrid" $ do
+   it "Should concatenate every line with a newline" $ do
+     (formatGrid ["abc", "def", "data"])  `shouldBe` "abc\ndef\ndata\n"
+
+ describe "findWord" $ do
+   it "Should find words that exist on the Grid" $ do
+     findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+     findWord grid "PERL" `shouldBe` Just "PERL"
+   it "Should not find words that do not exists on the Grid" $ do
+     findWord grid "HAMSTER" `shouldBe` Nothing
+
+ describe "findWord" $ do
+   it "Should find all the words that exist on the Grid" $ do
+     findWords grid languages `shouldBe` languages
+   it "Should not find words that do not exist on the Grid" $ do
+     findWords grid ["FRENCH", "SPANISH"] `shouldBe` []
